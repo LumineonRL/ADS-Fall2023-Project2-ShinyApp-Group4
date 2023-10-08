30bdc8f63f5c69564ea8a511f3b47a7a61b2bf25
LumineonRL <thebaconator25@aim.com>
2023-10-08 18:14:36 -0400

    Added Project file

--- lib/download_data.R
+++ lib/download_data.R
@@ -5,14 +5,16 @@ library(readr)
 library(here)
 library(stringr)
 
-DIRECTORY_TO_WRITE <- here("data")
-FILE_TO_WRITE <- "FimaNfipClaims.csv"
-CLAIMS_URL <- "https://www.fema.gov/api/open/v2/FimaNfipClaims"
-
-
-GET(CLAIMS_URL) %>%
-  extract2("content") %>%
-  rawToChar() %>%
-  fromJSON() %>%
-  extract2("FimaNfipClaims") %>%
-  write_csv(str_c(DIRECTORY_TO_WRITE, "/", FILE_TO_WRITE))
+download_data <- function() {
+  DIRECTORY_TO_WRITE <- here("data")
+  FILE_TO_WRITE <- "FimaNfipClaims.csv"
+  CLAIMS_URL <- "https://www.fema.gov/api/open/v2/FimaNfipClaims"
+  
+  
+  GET(CLAIMS_URL) %>%
+    extract2("content") %>%
+    rawToChar() %>%
+    fromJSON() %>%
+    extract2("FimaNfipClaims") %>%
+    write_csv(str_c(DIRECTORY_TO_WRITE, "/", FILE_TO_WRITE))
+}
