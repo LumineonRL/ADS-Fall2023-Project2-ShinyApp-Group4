b740c99a6e744bd48a2b69a3caddfd91e2db2d8a
LumineonRL <thebaconator25@aim.com>
2023-10-08 18:08:45 -0400

    Added download data function

---
+++ lib/download_data.R
@@ -0,0 +1,18 @@
+library(httr)
+library(jsonlite)
+library(magrittr)
+library(readr)
+library(here)
+library(stringr)
+
+DIRECTORY_TO_WRITE <- here("data")
+FILE_TO_WRITE <- "FimaNfipClaims.csv"
+CLAIMS_URL <- "https://www.fema.gov/api/open/v2/FimaNfipClaims"
+
+
+GET(CLAIMS_URL) %>%
+  extract2("content") %>%
+  rawToChar() %>%
+  fromJSON() %>%
+  extract2("FimaNfipClaims") %>%
+  write_csv(str_c(DIRECTORY_TO_WRITE, "/", FILE_TO_WRITE))
