984ab8d90dfc157046abf5fa1a866e5cb72049f2
LumineonRL <thebaconator25@aim.com>
2023-10-10 21:00:32 -0400

    Updated title

--- app/A1_ui_title.R
+++ app/A1_ui_title.R
@@ -2,7 +2,7 @@ ui_title <- function() {
   fluidPage(
     includeCSS("styles.css"),
     div(class = "title-container",
-        h1(class = "title-text", "Impacts of Disaster Insurance Claims")
+        h1(class = "title-text", "Impacts of Flood Insurance Claims")
     )
   )
 }
