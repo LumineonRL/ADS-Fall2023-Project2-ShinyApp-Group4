9ef2ef31918b5ef7a2779344f3adce30092cba2f
LumineonRL <thebaconator25@aim.com>
2023-10-10 21:35:46 -0400

    Updated references ui

--- app/A5_ui_references.R
+++ app/A5_ui_references.R
@@ -2,8 +2,8 @@ ui_references <- function() {
   tabPanel(
     "References",
     tags$h2("Data Sources"),
-    tags$a(href = "https://www.fema.gov/about/openfema/data-sets",
-           "OpenFEMA Data Set"),
+    tags$a(href = "https://www.fema.gov/openfema-data-page/fima-nfip-redacted-claims-v2",
+           "OpenFEMA Insurance Claim Data"),
     tags$h2("Contributors"),
     tags$p("Wenhe Chen"), tags$p("Daniel Lam"), tags$p("Heze Ma"), tags$p("Shreya Verma"), tags$p("Michael Wiley"),
     tags$h2("GitHub Repository"),
